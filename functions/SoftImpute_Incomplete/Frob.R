7ecda79108a4023a5211070402f0243fe0f8cae4
khaledfouda <khaledsfouda@gmail.com>
2024-01-26 09:54:21 -0500

    01/25 - 1

--- functions/SoftImpute_Incomplete/Frob.R
+++ functions/SoftImpute_Incomplete/Frob.R
@@ -1,7 +1,7 @@
-Frob=function(Uold,Dsqold,Vold,U,Dsq,V){
+Frob2=function(Uold,Dsqold,Vold,U,Dsq,V){
 denom=sum(Dsqold^2)
-utu=Dsq* (t(U)%*%Uold)
-vtv=Dsqold* (t(Vold)%*%V)
+utu=Dsq* (U%*%Uold)
+vtv=Dsqold* (Vold%*%V)
 uvprod= sum(diag(utu%*%vtv))
 num=denom+sum(Dsq^2) -2*uvprod
 num/max(denom,1e-9)
--- functions/SoftImpute_Incomplete/fit_covariate.R
+++ functions/SoftImpute_Incomplete/fit_covariate.R
@@ -12,7 +12,7 @@ function (y, X, J = 2, thresh = 1e-05, lambda=0,
   #  biats=attributes(x)[binames]
   #} else biats=NULL
   
-  timespent = rep(0,4)
+  # timespent = rep(0,4)
   if(!inherits(y,"dgCMatrix")) y=as(y,"dgCMatrix")
   irow=y@i
   pcol=y@p
@@ -21,7 +21,7 @@ function (y, X, J = 2, thresh = 1e-05, lambda=0,
   n <- n[1]
   nz=nnzero(y)
   #-------------------------------
-  start_time <- Sys.time()
+  #start_time <- Sys.time()
   # hat2 <- X %*% solve(t(X) %*% X) %*% t(X)
   # 
   #p = ncol(X)
@@ -36,28 +36,27 @@ function (y, X, J = 2, thresh = 1e-05, lambda=0,
     
    
     
-    timespent[1] = timespent[1] + as.numeric(difftime(Sys.time(), start_time,units = "secs"))
+  #timespent[1] = timespent[1] + as.numeric(difftime(Sys.time(), start_time,units = "secs"))
   
   # hat_sp[y == 0] = NA
   # hat_sp <- as(hat_sp, "Incomplete")
   #---------------------------------------------------
   warm=FALSE
+  clean.warm.start(warm.start)
   if(!is.null(warm.start)){
-    clean.warm.start(warm.start)
     #must have u,d and v components
     if(!all(match(c("u","d","v"),names(warm.start),0)>0))
       stop("warm.start does not have components u, d and v")
     warm=TRUE
     D=warm.start$d
     JD=sum(D>0)
+    lsq.sp <- warm.start$lsq.sp
     if(JD >= J){
       U=warm.start$u[,seq(J),drop=FALSE]
       V=warm.start$v[,seq(J),drop=FALSE]
       Dsq=D[seq(J)]
-      r = J
     }else{
       Dsq=c(D,rep(D[JD],J-JD))
-      r = length(Dsq)
       Ja=J-JD
       U=warm.start$u
       Ua=matrix(rnorm(n*Ja),n,Ja)
@@ -74,7 +73,7 @@ function (y, X, J = 2, thresh = 1e-05, lambda=0,
       Dsq=rep(1,J)# we call it Dsq because A=UD and B=VD and AB'=U Dsq V^T
       # update yfill[ynas] = 0
     }
-  
+  tU = t(U)
   r = length(Dsq)
   # initial beta estimates using yfill - yplus = yfill - xbeta
   ratio <- 1
@@ -83,11 +82,11 @@ function (y, X, J = 2, thresh = 1e-05, lambda=0,
   
   #-------------
   
-    start_time <- Sys.time()
+    #start_time <- Sys.time()
   while ((ratio > thresh)&(iter<maxit)) {
     iter <- iter + 1
     U.old=U
-    V.old=V
+    V.old= t(V)
     Dsq.old=Dsq
     #---------------------------------
     ## U step # S is yplus
@@ -104,9 +103,8 @@ function (y, X, J = 2, thresh = 1e-05, lambda=0,
     # 5
     #UtHat = t(U) %*% hat
     # 6
-    #print(paste("Execution time 1 is",round(as.numeric(difftime(Sys.time(), start_time,units = "secs")),5), "seconds"))
-    tU = t(U)
     tBD = t(BD)
+    #print(paste("Execution time 1 is",round(as.numeric(difftime(Sys.time(), start_time,units = "secs")),5), "seconds"))
     B =    (tU - (tU %*% hat_sp)) %*% S         + 
       (tBD -  (((tU%*% hat) %*% U) %*% tBD) )
     #B=t(t(U)%*%S)+BD
@@ -138,22 +136,22 @@ function (y, X, J = 2, thresh = 1e-05, lambda=0,
     A = (SV - hat_sp %*% SV) + (AD - hat %*% AD)
     # timespent[2] = timespent[2] + as.numeric(difftime(Sys.time(), start_time,units = "secs"))
     #A=S%*%V+AD
-    Dsq_scaled <- sapply(Dsq, function(d) d / (d + lambda))
-    
-    if(lambda>0) A = t(t(A) * (Dsq/(Dsq+lambda))) #UD(A,Dsq/(Dsq+lambda),n)
+
+    if(lambda>0) A = (t(A) * (Dsq/(Dsq+lambda))) #UD(A,Dsq/(Dsq+lambda),n)
     Asvd=  fast.svd(as.matrix(A))
-    U=Asvd$u
+    U= t(Asvd$u)
     Dsq=Asvd$d
     V=V %*% Asvd$v
     r = length(Dsq)
+    tU = t(U)
     # update yhat = UDV then yfill[ynas] = yhat[ynas] + xbeta[ynas]
     # compute beta estimates and yplus (S) = yfill - xbeta
     #------------------------------------------------------------------------------
-    ratio=Frob(U.old,Dsq.old,V.old,U,Dsq,V)
+    ratio=Frob2(U.old,Dsq.old,V.old,tU,Dsq,V)
     if(trace.it) cat(iter, ":", "obj",format(round(obj,5)),"ratio", ratio, "\n")
   }
-    timespent[3] = timespent[3] + as.numeric(difftime(Sys.time(), start_time,units = "secs"))
-    start_time <- Sys.time()
+    #timespent[3] = timespent[3] + as.numeric(difftime(Sys.time(), start_time,units = "secs"))
+    #start_time <- Sys.time()
   if(iter==maxit)warning(paste("Convergence not achieved by",maxit,"iterations"))
   if(lambda>0&final.svd){
     AD=UD(U,Dsq,n)
@@ -174,9 +172,10 @@ function (y, X, J = 2, thresh = 1e-05, lambda=0,
     }
   }
   J=min(sum(Dsq>0)+1,J)
-  out=list(u=U[, seq(J)], d=Dsq[seq(J)], v=V[,seq(J)], lambda=lambda, rank=J)
+  out=list(u=U[, seq(J)], d=Dsq[seq(J)], v=V[,seq(J)], lambda=lambda, rank=J,
+           lsq.sp = lsq.sp)
   #attributes(out)=c(attributes(out),list(lambda=lambda,call=this.call),biats)
-  timespent[4] = timespent[4] + as.numeric(difftime(Sys.time(), start_time,units = "secs"))
-  print(round(timespent, 3))
+  #timespent[4] = timespent[4] + as.numeric(difftime(Sys.time(), start_time,units = "secs"))
+  #print(round(timespent, 3))
   out
 }
