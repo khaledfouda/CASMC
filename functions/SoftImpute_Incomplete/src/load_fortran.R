84de2328c1081571d0d4eb7e88cfd1922883a19e
khaledfouda <khaledsfouda@gmail.com>
2024-02-01 12:41:42 -0500

    02/01 - 1

--- functions/SoftImpute_Incomplete/src/load_fortran.R
+++ functions/SoftImpute_Incomplete/src/load_fortran.R
@@ -3,3 +3,5 @@
 
 dyn.load("./functions/SoftImpute_Incomplete/src/suvC.so")
 dyn.load("./functions/SoftImpute_Incomplete/src/sparse_prod.so")
+dyn.unload("./functions/SoftImpute_Incomplete/src/sparse_prod.so")
+
--- functions/SoftImpute_Incomplete/src/sparse_prod.f90
+++ functions/SoftImpute_Incomplete/src/sparse_prod.f90
@@ -6,36 +6,29 @@ subroutine sparse_prod(n, m, r, H, sp, si, sx, result)
     double precision, intent(in) :: H(n,n), sx(r)
     double precision, intent(out) :: result(r)
 
-    integer :: j, jstart, jend, ii, counter, hrow
-    double precision, dimension(n) :: h_sub_row, sx_sub
+    integer :: j, jstart, jend, ii, i, counter, hrow
+    double precision, dimension(n) :: h_sub_row
 
     counter = 1
     do j = 1, m
-        jstart = sp(j) 
-        jend = sp(j + 1) -1
+        jstart = sp(j) + 1
+        jend = sp(j + 1)
 
         if (jstart <= jend) then
-            sx_sub = sx(jstart:jend)
             do ii = jstart, jend
-                hrow = si(ii) 
-                call extract_row(H, hrow, n, h_sub_row)
-                result(counter) = dot_product(h_sub_row, sx_sub)
+                hrow = si(ii) + 1
+
+                ! Extract the sub-row from H corresponding to hrow
+                h_sub_row = H(hrow, :)
+
+                ! Compute the dot product for the specific elements
+                result(counter) = 0.0
+                do i = jstart, jend
+                    result(counter) = result(counter) + h_sub_row(si(i) + 1) * sx(i)
+                end do
+                
                 counter = counter + 1
             end do
         end if
     end do
 end subroutine sparse_prod
-
-subroutine extract_row(H, row, n, h_sub_row)
-    implicit none
-
-    integer, intent(in) :: row, n
-    double precision, intent(in) :: H(n, n)
-    double precision, intent(out), dimension(n) :: h_sub_row
-
-    integer :: i
-
-    do i = 1, n
-        h_sub_row(i) = H(row, i)
-    end do
-end subroutine extract_row
