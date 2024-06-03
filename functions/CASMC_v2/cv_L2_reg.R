7a6d1a191b5f6c897631975e5d52ce355e3d419d
khaledfouda <khaledsfouda@gmail.com>
2024-06-03 15:09:21 -0400

    06/03 - 1

---
+++ functions/CASMC_v2/cv_L2_reg.R
@@ -0,0 +1,116 @@
+CASMC_cv_L2 <-
+   function(y_train,
+            # y_train is expected to be Incomplete
+            X,
+            y_valid,
+            # y_valid is a vector
+            W_valid,
+            y = NULL,
+            # y: a final full-fit if provided. Expected to be Incomplete
+            error_function = error_metric$rmse,
+            # tuning parameters for lambda
+            lambda.factor = 1 / 4,
+            lambda.init = NULL,
+            n.lambda = 20,
+            # tuning parameters for J
+            rank.init = 2,
+            rank.limit = 30,
+            rank.step = 2,
+            pct = 0.98,
+            # laplacian parameters
+            lambda.a = 0,
+            S.a = NULL,
+            lambda.b = 0,
+            S.b = NULL,
+            # stopping criteria
+            early.stopping = 1,
+            thresh = 1e-6,
+            maxit = 100,
+            # trace parameters
+            trace = FALSE,
+            print.best = TRUE,
+            quiet = FALSE,
+            # initial values.
+            warm = NULL,
+            # L2 parameters
+            lambda.beta.grid = "default",
+            track = FALSE,
+            max_cores = 8,
+            # seed
+            seed = NULL) {
+      if (identical(lambda.beta.grid, "default"))
+         lambda.beta.grid = sqrt((ncol(y_train) * ncol(X)) / (nrow(y_train))) *  seq(0, 10, length.out = 20)
+      
+      num_cores = length(lambda.beta.grid)
+      if (length(lambda.beta.grid) > max_cores)
+         num_cores <-
+         min(max_cores, ceiling(length(lambda.beta.grid) / 2))
+      print(paste("Running on", num_cores, "cores."))
+      X_r <-  reduced_hat_decomp(X)
+      
+      results <- mclapply(lambda.beta.grid, function(lambda.beta) {
+         Xterms = GetXterms(X, lambda.beta)
+         fiti = CASMC_cv_nuclear(
+            y_train = y_train,
+            X = X,
+            y_valid = y_valid,
+            W_valid = W_valid,
+            y = y,
+            Xterms = Xterms,
+            X_r = X_r,
+            r = NULL,
+            error_function = error_function,
+            lambda.factor = lambda.factor,
+            lambda.init = lambda.init,
+            n.lambda = n.lambda,
+            trace = trace,
+            print.best = print.best,
+            early.stopping = early.stopping,
+            thresh = thresh,
+            maxit = maxit,
+            rank.init = rank.init,
+            rank.limit = rank.limit,
+            rank.step = rank.step,
+            pct = pct,
+            warm = NULL,
+            lambda.a = lambda.a,
+            S.a = S.a,
+            lambda.b = lambda.b,
+            S.b = S.b,
+            quiet = quiet,
+            seed = seed
+         )
+         fiti$lambda.beta = lambda.beta
+         fiti
+      }, mc.cores = num_cores, mc.cleanup = TRUE)
+      
+      
+      best_fit <-
+         results[[which.min(sapply(results, function(x)
+            x$error))]]
+      
+      if (track) {
+         sapply(results, function(x)
+            print(
+               paste0(
+                  "lambda.beta = ",
+                  x$lambda.beta,
+                  " - Val Err = ",
+                  round(x$error, 5)
+               )
+            ))
+      }
+      if (print.best)
+         print(
+            paste(
+               "Best fit: lambda_beta = ",
+               best_fit$lambda.beta,
+               " - Validation Error: ",
+               best_fit$error
+            )
+         )
+      
+      return(best_fit)
+      
+      
+   }
---
+++ functions/CASMC_v2/helpers.R
@@ -0,0 +1,166 @@
+reduced_hat_decomp <- function(X, tol = 1e-2, pct=NULL) {
+   X_svd = fast.svd(X, tol)
+   if(! is.null(pct)){
+      stopifnot(pct >= 0.5 && pct < 1)
+      var_contrib = cumsum(X_svd$d / sum(X_svd$d))
+      n_d <- sum(var_contrib < pct)
+      X_svd$d <- X_svd$d[1:n_d]
+      X_svd$u <- X_svd$u[,1:n_d]
+      X_svd$v <- X_svd$v[,1:n_d]
+   }
+   X = X_svd$u %*% (X_svd$d * t(X_svd$v))
+   rank = length(X_svd$d)
+   print(paste("Rank of X changed from", min(dim(X)),"to",rank))
+   Q <- qr.Q(Matrix::qr(X))
+   H <- Q %*% t(Q)
+   #svdH <- fast.svd(H, tol)
+   svdH <- tryCatch({
+   irlba(H, nu = rank, nv = rank, tol = tol)
+   },
+   error = function(e){
+      message(paste("SvdH:", e))
+      svd_trunc_simple(H, rank)
+   })
+   svdH$u = svdH$u
+   svdH$v = svdH$d * t(svdH$v)
+   svdH$d <- H <- Q <- NULL
+   return(list(X = X, svdH = svdH, rank = rank))
+}
+med_scale <- function(x) (x - median(x)) / IQR(x)
+
+reduced_hat_decomp.H <- function(X) {
+   qrX = Matrix::qr(X)
+   rank = qrX$rank
+   Q <- qr.Q(qrX)
+   H <- Q %*% t(Q)
+   svdH <- tryCatch({
+      irlba(H, nu = rank, nv = rank, tol = 1e-5)
+   },
+   error = function(e){
+      message(paste("SvdH:", e))
+      svd_trunc_simple(H, rank)
+   })
+   list(
+      u = svdH$u,
+      v = svdH$d * t(svdH$v),
+      rank = rank
+   )
+}
+
+UD = function(U, D, n = nrow(U)) {
+   U * outer(rep(1, n), D, "*")
+}
+GetXterms <- function(X, lambda=0) {
+   svdX = fast.svd(X)
+   Ux = svdX$u
+   Vx = svdX$d * t(svdX$v)
+   X0 = ginv(t(Vx) %*% Vx + diag(lambda, ncol(X))) %*% t(Vx)
+   X1 = X0 %*% t(Ux) # k by n
+   X2 = X0 %*% Vx # k by k
+   list(X1 = X1, X2 = X2)
+}
+lambda0.cov_splr <- function(Y,
+                             svdH,
+                             tol = 1e-1,
+                             max_it = 30) {
+   yplus = Y - svdH$u %*% (svdH$v %*% Y)
+   propack.svd(as.matrix(yplus), 1, opts = list(kmax = max_it))$d[1]
+   # if (inherits(Y, "dgCMatrix") & (dim(Y)[1] == dim(Y)[2]) ) {
+   #    eigs(yplus, k = 1, which = "LM")$values
+   # } else
+} 
+
+computeLaplacian <- function(S, normalized = TRUE) {
+   d <- rowSums(S)
+   if (normalized) {
+      d_inv_sqrt <- 1 / sqrt(d)
+      L <- diag(1, nrow(S)) - (d_inv_sqrt %*% t(d_inv_sqrt)) * S
+   } else {
+      L <- diag(d) - S
+   }
+   L[L==0] <- NA
+   L <- as(L, "Incomplete")
+   return(L)
+}
+update_chol <- function(chol_M, D_vector) {
+   diag_update <- diag(sqrt(as.vector(D_vector)))  # Square root for Cholesky update
+   return(chol_M + diag_update)  
+}
+
+
+suvC <-
+   function(u, v, irow, pcol) {
+      dd = dim(u)
+      nnrow = as.integer(dd[1])
+      nncol = as.integer(nrow(v))
+      nrank = dd[2]
+      storage.mode(u) = "double"
+      storage.mode(v) = "double"
+      storage.mode(irow) = "integer"
+      storage.mode(pcol) = "integer"
+      nomega = as.integer(length(irow))
+      .Fortran(
+         "suvC",
+         nnrow,
+         nncol,
+         nrank,
+         u,
+         v,
+         irow,
+         pcol,
+         nomega,
+         r = double(nomega),
+         PACKAGE = "softImpute"
+      )$r
+   }
+
+
+generate_similarity_matrix <- function(n, seed = NULL) {
+   if(! is.null(seed)) set.seed(seed)
+   matrix <- matrix(0, n, n)
+   matrix[upper.tri(matrix)] <- sample(c(0, 1), sum(upper.tri(matrix)), replace = TRUE)
+   matrix <- matrix + t(matrix) # make it symmetric
+   diag(matrix) <- 1
+   return(matrix)
+}
+
+
+
+
+# sparse_prod <-
+#   function(n,m,r,H,sp,si,sx){
+#     storage.mode(H)="double"
+#     storage.mode(sx)="double"
+#     storage.mode(si)="integer"
+#     storage.mode(sp)="integer"
+#     storage.mode(n)="integer"
+#     storage.mode(m)="integer"
+#     storage.mode(r)="integer"
+#     result = double(r)
+#
+#     .Fortran("sparse_prod",
+#              n,m,r,H,sp,si,sx
+#     )$result
+#   }
+#
+#
+# sparse_prod <- function(n, m, r, H, sp, si, sx) {
+#   # Ensure correct data types
+#   storage.mode(H) = "double"
+#   storage.mode(sx) = "double"
+#   storage.mode(si) = "integer"
+#   storage.mode(sp) = "integer"
+#   storage.mode(n) = "integer"
+#   storage.mode(m) = "integer"
+#   storage.mode(r) = "integer"
+#
+#   # Preallocate the result vector
+#   result = double(r)
+#
+#   # Call the Fortran subroutine
+#   .Fortran("sparse_prod",
+#            as.integer(n), as.integer(m), as.integer(r),
+#            as.double(H), as.integer(sp), as.integer(si),
+#            as.double(sx), result = as.double(result)
+#   )$result
+# }
