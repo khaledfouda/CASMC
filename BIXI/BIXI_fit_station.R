f860b647e0cb19e8457acbdb2c0bb9149ac00fdd
khaledfouda <khaledsfouda@gmail.com>
2024-06-02 21:23:13 -0400

    05/26 - 1

--- BIXI/BIXI_fit_station.R
+++ BIXI/BIXI_fit_station.R
@@ -115,11 +115,12 @@ for (case in 0:4) {
 }
 
 
+
 simpute_fit <- simpute.cv(
-  Y_train = as.matrix(model.dat$fit_data$train),
-  y_valid = model.dat$fit_data$valid,
-  W_valid = model.dat$fit_data$W_valid,
-  y = model.dat$Y,
+  Y_train = as.matrix(model.dat$splits$train),
+  y_valid = model.dat$splits$valid,
+  W_valid = model.dat$masks$valid,
+  y = as.matrix(model.dat$depart),
   n.lambda = 20,
   trace = FALSE,
   print.best = TRUE,
@@ -136,13 +137,13 @@ simpute_fit <- simpute.cv(
 
 all_res  |>
   rbind(print_performance(model.dat, simpute_fit, error_metric$rmse, TRUE, "SoftImpute", F, 3)) |> 
-  arrange(XBeta) |>
-  mutate(XBeta = paste0("[", 1:nrow(all_res), "]", XBeta)) |>
-  arrange(M) |>
-  mutate(M = paste0("[", 1:nrow(all_res), "]", M)) |>
-  arrange(Y) |>
-  mutate(Y = paste0("[", 1:nrow(all_res), "]", Y)) |>
-  dplyr::select(-Y) |>
+  #arrange(XBeta) |>
+  #mutate(XBeta = paste0("[", 1:nrow(all_res), "]", XBeta)) |>
+  #arrange(M) |>
+  #mutate(M = paste0("[", 1:nrow(all_res), "]", M)) |>
+  #arrange(Y) |>
+  #mutate(Y = paste0("[", 1:nrow(all_res), "]", Y)) |>
+  dplyr::select(-Y, -M, -XBeta) |>
   arrange(train) |>
   mutate(train = paste0("[", 1:nrow(all_res), "]", train)) |>
   arrange(test) |>
@@ -156,137 +157,137 @@ kable(all_res, format = "simple") |>  print()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-#-----------------------------------------------------------------------------
-aresults <- list()
-i = b = 1
-
-X_r <- reduced_hat_decomp(model.dat$X, 1)
-X_r$rank
-
-sparm = list(NULL,NULL,"")
-for(sparm in list(list(NULL,NULL,""),
-                  list(model.dat$similarity.A, NULL, "_with_time_laplacian")
-                  #list(NULL, model.dat$sim_col),
-                  #list(model.dat$sim_row, model.dat$sim_col)
-                  )
-    ){
-
-  X <- model.dat$X[,c(1), drop=FALSE]|> scale()
-  #X = cbind(X[,1:4]^2)
-  #------------------------
-  X <- cbind(
-    #X,
-    scale(X[,1:3])^2,
-    log(X[,1, drop=FALSE]+abs(min(X[,1]))+1)
-    #matrix(X[,4]>0,ncol= 1)
-  )  
-  
-  #---------------------
-  cor(X)
-  # for(b in 1:5){
-  model.dat <- load_bixi_dat(transpose = F, scale_response = F, scale_covariates = F,
-                             testp = 0.5, validp = 0.2, seed=b)$model
-  model.dat$X <- model.dat$X |> scalers("minmax")
-  model.dat$X <- remove_collinear_cols(model.dat$X, 0.7)
-  model.dat$X <- reduced_hat_decomp(model.dat$X, .01, 0.98)$X
-  #X <-  model.dat$X[,-c(1,5,9,12,16)] |> scalers("minmax")
-  #X <- X[,-c(5,7,9)]
-  #X_r = reduced_hat_decomp(X,.01, 0.99)
-  #X = X[,1:5]
-  #X[1,]
-  #dim(X_r$X)
-  #X_r$rank
-  #X <- model.dat$X[,c(1,2, 3,4,6,7,8)]
-  
-  results <- CASMC_var_selection(
-    y_train = model.dat$splits$train,
-    y_valid = model.dat$splits$valid,
-    Y = model.dat$depart,
-    X = model.dat$X,
-    W_valid = model.dat$masks$valid,
-    track = TRUE
-  )
-  
-  results |> 
-    select(-variables) |> 
-    arrange(validation_error)
-  
-  #-----------------------------------------------------------------------------
-  start_time = Sys.time()
-  best_fit = CASMC_cv_rank(
-    y_train = model.dat$splits$train,
-    X = X,#[,1:5, drop=FALSE],
-    
-    y_valid = model.dat$splits$valid@x,
-    W_valid = model.dat$masks$valid ,
-    #y = model.dat$depart,
-    trace = F,
-    max_cores = 30,
-    thresh = 1e-6,
-    lambda.a = 0.01,
-    S.a = sparm[[1]],
-    lambda.b = 0.2,
-    S.b = sparm[[2]],
-    #n.lambda = n.lambda,
-    #rank.limit = rank.limit,
-    maxit = 200,
-    r_min = 0,
-    rank.init = 2,
-    rank.step = 4,
-    print.best = TRUE,
-    seed = 2023,
-    track  = T
-  )
-  best_fit$rank_M
-  test_error <- error_metric$rmse
-  fit1 = best_fit$fit
-  sout = best_fit
-  # get estimates and validate
-  sout$M = unsvd(fit1)
-  sout$beta =  fit1$beta
-  apply(sout$beta, 1, summary) |> print()
-  
-  sout$estimates = sout$M + X %*% (sout$beta)
-  
-  hist(sout$beta[1,])
-  
-  plot(1:length(sout$estimates), as.vector(sout$estimates))
-  
-  #plot(1:ncol(sout$beta), sout$beta[3,])
-  
-  if(b > 1) old_results = results
-  results = list(model = paste0("CASMC_rank_all",sparm[[3]]))
-  results$time = round(as.numeric(difftime(Sys.time(), start_time, units = "secs")))
-  #results$lambda.1 = NA#sout$lambda.beta |> round(3)
-  #results$lambda.2 = sout$lambda |> round(3)
-  results$error.test = test_error(sout$estimates[model.dat$masks$test == 0],
-                                  model.dat$splits$test@x) |> round(5)
-  results$error.train = test_error(sout$estimates[model.dat$masks$test == 1 & model.dat$masks$obs == 1],
-                                   model.dat$depart[model.dat$masks$test == 1& model.dat$masks$obs == 1]) |> 
-    round(5)
-  results$error.valid = test_error(sout$estimates[model.dat$masks$valid == 0],
-                                   model.dat$splits$valid@x) |> round(5)
-  
-  results$rank = qr(sout$estimates)$rank
-  if(b > 1)
-  results[-1] = mapply(sum, old_results[-1], results[-1])
-  }
-  results[-1] = mapply(function(x)x/5, results[-1])
-  
-  
-  aresults[[i]] <- results
-  i = i +1
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# #-----------------------------------------------------------------------------
+# aresults <- list()
+# i = b = 1
+# 
+# X_r <- reduced_hat_decomp(model.dat$X, 1)
+# X_r$rank
+# 
+# sparm = list(NULL,NULL,"")
+# for(sparm in list(list(NULL,NULL,""),
+#                   list(model.dat$similarity.A, NULL, "_with_time_laplacian")
+#                   #list(NULL, model.dat$sim_col),
+#                   #list(model.dat$sim_row, model.dat$sim_col)
+#                   )
+#     ){
+# 
+#   X <- model.dat$X[,c(1), drop=FALSE]|> scale()
+#   #X = cbind(X[,1:4]^2)
+#   #------------------------
+#   X <- cbind(
+#     #X,
+#     scale(X[,1:3])^2,
+#     log(X[,1, drop=FALSE]+abs(min(X[,1]))+1)
+#     #matrix(X[,4]>0,ncol= 1)
+#   )  
+#   
+#   #---------------------
+#   cor(X)
+#   # for(b in 1:5){
+#   model.dat <- load_bixi_dat(transpose = F, scale_response = F, scale_covariates = F,
+#                              testp = 0.5, validp = 0.2, seed=b)$model
+#   model.dat$X <- model.dat$X |> scalers("minmax")
+#   model.dat$X <- remove_collinear_cols(model.dat$X, 0.7)
+#   model.dat$X <- reduced_hat_decomp(model.dat$X, .01, 0.98)$X
+#   #X <-  model.dat$X[,-c(1,5,9,12,16)] |> scalers("minmax")
+#   #X <- X[,-c(5,7,9)]
+#   #X_r = reduced_hat_decomp(X,.01, 0.99)
+#   #X = X[,1:5]
+#   #X[1,]
+#   #dim(X_r$X)
+#   #X_r$rank
+#   #X <- model.dat$X[,c(1,2, 3,4,6,7,8)]
+#   
+#   results <- CASMC_var_selection(
+#     y_train = model.dat$splits$train,
+#     y_valid = model.dat$splits$valid,
+#     Y = model.dat$depart,
+#     X = model.dat$X,
+#     W_valid = model.dat$masks$valid,
+#     track = TRUE
+#   )
+#   
+#   results |> 
+#     select(-variables) |> 
+#     arrange(validation_error)
+#   
+#   #-----------------------------------------------------------------------------
+#   start_time = Sys.time()
+#   best_fit = CASMC_cv_rank(
+#     y_train = model.dat$splits$train,
+#     X = X,#[,1:5, drop=FALSE],
+#     
+#     y_valid = model.dat$splits$valid@x,
+#     W_valid = model.dat$masks$valid ,
+#     #y = model.dat$depart,
+#     trace = F,
+#     max_cores = 30,
+#     thresh = 1e-6,
+#     lambda.a = 0.01,
+#     S.a = sparm[[1]],
+#     lambda.b = 0.2,
+#     S.b = sparm[[2]],
+#     #n.lambda = n.lambda,
+#     #rank.limit = rank.limit,
+#     maxit = 200,
+#     r_min = 0,
+#     rank.init = 2,
+#     rank.step = 4,
+#     print.best = TRUE,
+#     seed = 2023,
+#     track  = T
+#   )
+#   best_fit$rank_M
+#   test_error <- error_metric$rmse
+#   fit1 = best_fit$fit
+#   sout = best_fit
+#   # get estimates and validate
+#   sout$M = unsvd(fit1)
+#   sout$beta =  fit1$beta
+#   apply(sout$beta, 1, summary) |> print()
+#   
+#   sout$estimates = sout$M + X %*% (sout$beta)
+#   
+#   hist(sout$beta[1,])
+#   
+#   plot(1:length(sout$estimates), as.vector(sout$estimates))
+#   
+#   #plot(1:ncol(sout$beta), sout$beta[3,])
+#   
+#   if(b > 1) old_results = results
+#   results = list(model = paste0("CASMC_rank_all",sparm[[3]]))
+#   results$time = round(as.numeric(difftime(Sys.time(), start_time, units = "secs")))
+#   #results$lambda.1 = NA#sout$lambda.beta |> round(3)
+#   #results$lambda.2 = sout$lambda |> round(3)
+#   results$error.test = test_error(sout$estimates[model.dat$masks$test == 0],
+#                                   model.dat$splits$test@x) |> round(5)
+#   results$error.train = test_error(sout$estimates[model.dat$masks$test == 1 & model.dat$masks$obs == 1],
+#                                    model.dat$depart[model.dat$masks$test == 1& model.dat$masks$obs == 1]) |> 
+#     round(5)
+#   results$error.valid = test_error(sout$estimates[model.dat$masks$valid == 0],
+#                                    model.dat$splits$valid@x) |> round(5)
+#   
+#   results$rank = qr(sout$estimates)$rank
+#   if(b > 1)
+#   results[-1] = mapply(sum, old_results[-1], results[-1])
+#   }
+#   results[-1] = mapply(function(x)x/5, results[-1])
+#   
+#   
+#   aresults[[i]] <- results
+#   i = i +1
   #------------------------------
 #=========================================================================================
 #   
